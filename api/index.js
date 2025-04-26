503262c8a69e06f49e95b96be10bd370f847e08e
Matheus Souto <matheushsouto@gmail.com>
2025-04-26 17:52:55 -0300

    chore: change spa function

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import('../dist/jacto-tmdb-hexagonal/server/server.mjs')
-  .then(module => module.app)
-  .catch(error => {
-    throw error;
-  });
-export default async (req, res) => {
-  const { app } = await import('../dist/jacto-tmdb-hexagonal/server/server.mjs');
-  return app(req, res);
-};
